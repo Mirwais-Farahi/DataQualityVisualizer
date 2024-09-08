a8234d5d27e6b3c827974acb2fce07488d55273b
Mirwais Farahi <waisfarahi@example.com>
2024-09-08 15:41:38 +0430

    Update File

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-
-# To use a consistent encoding
-from codecs import open
 from os import path
 
-# The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -19,7 +13,7 @@ setup(
     author='Mirwais Farahi',
     author_email='waisfarahi@example.com',
     description='Python module for automated data quality assurance and visualization to detect outliers and anomalies.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/mirwais-farahi/DataQualityVisualizer',
     license="MIT",
@@ -28,8 +22,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-
-    packages=["data_quality_visualizer"],
     include_package_data=True,
-    install_requires=["pandas","numpy"],
+    install_requires=["pandas", "numpy"],
 )
