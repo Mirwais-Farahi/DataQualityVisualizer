36b8d103780d07d842f2012eda6e14ddf11780c0
Mirwais Farahi <waisfarahi@example.com>
2024-09-08 15:47:51 +0430

    Update File

--- data_quality_visualizer/data_quality_visualizer.py
+++ data_quality_visualizer/data_quality_visualizer.py
@@ -7,29 +7,26 @@ class DataQualityVisualizer:
         
     def check_null_values(self):
         """
-        Checks for null values in the DataFrame and prints the result.
+        Checks for null values in the DataFrame and returns the result.
 
-        This method evaluates whether any null values are present in the DataFrame. 
-        It prints the test result and the total number of null values if any are found.
+        This method evaluates whether any null values are present in the DataFrame.
+        It returns a dictionary with the test result and the total number of null values if any are found.
 
         Returns:
-        None
+        dict: A dictionary with the keys 'test_status' and 'null_count'.
+              'test_status' is either 'Passed' or 'Failed', and 'null_count' contains
+              the total number of null values if the test failed.
         """
-        print("[=================================================================]")
-        print("")
-        
-        # Check for the presence of null values in the DataFrame
-        has_nulls = self.df.isnull().values.any()
-        
-        if has_nulls:
-            # Calculate the total number of null values
-            null_sum = self.df.isnull().sum().sum()
-            print("TEST CASE NULL VALUES: Failed")
-            print(f"Total number of null values: {null_sum}")
-            print("Please address these null values to ensure data completeness.")
+        result = {}
+
+        has_null_values = self.df.isnull().values.any()
+
+        if has_null_values:
+            null_count = self.df.isnull().sum().sum()
+            result['test_status'] = 'Failed'
+            result['null_count'] = null_count
         else:
-            print("TEST CASE NULL VALUES: Passed")
-            print("No null values found. The dataset is complete.")
-        
-        print("")
-        print("[=================================================================]")
+            result['test_status'] = 'Passed'
+            result['null_count'] = 0
+
+        return result
